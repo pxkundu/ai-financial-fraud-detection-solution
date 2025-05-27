323a33adc6a34120bced8c50e92938399cc5b21f
pxkundu <pxkundu2@shockers.wichita.edu>
2025-05-27 12:27:37 -0400

    Updated github page with output data 4

--- dataiku/scripts/update_index.py
+++ dataiku/scripts/update_index.py
@@ -1,4 +1,5 @@
 import markdown2
+import os
 
 def update_index():
     # Read the test output markdown
@@ -9,9 +10,10 @@ def update_index():
     html_content = markdown2.markdown(md_content, extras=['tables', 'fenced-code-blocks'])
     
     # Append to index.html
-    with open('docs/index.html', 'a') as f:
+    with open('../docs/index.html', 'a') as f:
         f.write('\n\n## Feature Engineering Test Results\n\n')
         f.write(html_content)
+        f.write('\n</div></div></div></body></html>')
 
 if __name__ == '__main__':
     update_index() 
