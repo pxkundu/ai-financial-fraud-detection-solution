2b8a1656ce44a3361d1a90fb9688a82d5394c71e
pxkundu <pxkundu2@shockers.wichita.edu>
2025-05-27 11:20:05 -0400

    Updated github page with output data

---
+++ dataiku/scripts/update_index.py
@@ -0,0 +1,17 @@
+import markdown2
+
+def update_index():
+    # Read the test output markdown
+    with open('tests/testOutput.md', 'r') as f:
+        md_content = f.read()
+    
+    # Convert markdown to HTML
+    html_content = markdown2.markdown(md_content, extras=['tables', 'fenced-code-blocks'])
+    
+    # Append to index.html
+    with open('docs/index.html', 'a') as f:
+        f.write('\n\n## Feature Engineering Test Results\n\n')
+        f.write(html_content)
+
+if __name__ == '__main__':
+    update_index() 
